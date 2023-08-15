c51fedd65229680310c2d95e9508b0770c2d493b
Jan Godlewski <Jan.Godlewski@motorolasolutions.com>
2023-08-15 10:11:10 +0200

    Add fields with information about note id and if it has image

--- lib/features/notes/domain/entity/note.dart
+++ lib/features/notes/domain/entity/note.dart
@@ -7,11 +7,19 @@ part 'note.g.dart';
 class Note{
   final String title;
   final String content;
+  @JsonKey(defaultValue: false)
+  final bool hasImage;
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  String? id;
   @JsonKey(includeFromJson: false, includeToJson: false)
   XFile? image;
 
-  Note({required this.title, required this.content});
+  Note({required this.title, required this.content, required this.hasImage});
 
-  factory Note.fromJson(Map<String, dynamic> json) => _$NoteFromJson(json);
+  factory Note.fromJson(String id, Map<String, dynamic> json){
+   Note note = _$NoteFromJson(json);
+   note.id = id;
+   return note;
+  }
   Map<String, dynamic> toJson() => _$NoteToJson(this);
 }
--- lib/features/notes/domain/entity/note.g.dart
+++ lib/features/notes/domain/entity/note.g.dart
@@ -9,9 +9,11 @@ part of 'note.dart';
 Note _$NoteFromJson(Map<String, dynamic> json) => Note(
       title: json['title'] as String,
       content: json['content'] as String,
+      hasImage: json['hasImage'] as bool? ?? false,
     );
 
 Map<String, dynamic> _$NoteToJson(Note instance) => <String, dynamic>{
       'title': instance.title,
       'content': instance.content,
+      'hasImage': instance.hasImage,
     };
